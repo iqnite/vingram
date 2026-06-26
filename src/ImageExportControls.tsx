65ead3755cd05113b24d1a2c2fccb6bb648f7f96
iqnite <iqnitetechnologies@gmail.com>
2026-06-26 13:33:03 +0200

    Add SVG export

--- src/ImageExportControls.tsx
+++ src/ImageExportControls.tsx
@@ -1,34 +1,42 @@
 import { useCallback } from "react";
 import { useReactFlow } from "@xyflow/react";
-import { toPng, toBlob } from "html-to-image";
+import { toPng, toBlob, toSvg } from "html-to-image";
 import "./App.css";
 import DropdownMenu from "./DropdownMenu";
 
 export default function ImageExportControls() {
   const { fitView, getNodes } = useReactFlow();
 
-  const onDownload = useCallback(async () => {
-    fitView({
-      padding: 0.2,
-      nodes: getNodes(),
-      duration: 0,
-    });
+  const onDownload = useCallback(
+    async (format: "png" | "svg") => {
+      fitView({
+        padding: 0.2,
+        nodes: getNodes(),
+        duration: 0,
+      });
 
-    await new Promise((resolve) => setTimeout(resolve, 50));
+      await new Promise((resolve) => setTimeout(resolve, 50));
 
-    const reactFlowElement = document.querySelector(
-      ".react-flow__viewport",
-    ) as HTMLElement;
+      const reactFlowElement = document.querySelector(
+        ".react-flow__viewport",
+      ) as HTMLElement;
 
-    toPng(reactFlowElement, {
-      backgroundColor: "#ffffff",
-    }).then((dataUrl) => {
-      const link = document.createElement("a");
-      link.download = "my-drawing.png";
-      link.href = dataUrl;
-      link.click();
-    });
-  }, [fitView, getNodes]);
+      (format === "png" ? toPng : toSvg)(
+        reactFlowElement,
+        format === "png"
+          ? {
+              backgroundColor: "#ffffff",
+            }
+          : {},
+      ).then((dataUrl) => {
+        const link = document.createElement("a");
+        link.download = `my-drawing.${format}`;
+        link.href = dataUrl;
+        link.click();
+      });
+    },
+    [fitView, getNodes],
+  );
 
   const onCopyToClipboard = useCallback(async () => {
     fitView({
@@ -60,7 +68,8 @@ export default function ImageExportControls() {
         title="Export"
         options={[
           { name: "Copy to clipboard", callback: onCopyToClipboard },
-          { name: "Download PNG", callback: onDownload },
+          { name: "Download PNG", callback: () => onDownload("png") },
+          { name: "Download SVG", callback: () => onDownload("svg") },
         ]}
       />
     </span>
